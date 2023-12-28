7a9de07b05e4bee0d50d490bf070e4ad766284e4
Lukas <lucidLuckylee@users.noreply.github.com>
2023-12-28 17:51:30 +0100

    Add new year message; prettier print

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,25 @@ fn main() -> Result<(), std::io::Error> {
    // Verify proof
    let chain_state = verify(public_input_bytes, proof_bytes);
 
-   println!("ChainState: {}", serde_json::to_string(&chain_state.unwrap()).unwrap());
+   let new_years_message = r#"
+   ======================================================================
+   |   ______ _______  ______  _____  _______ __  __ __   _ _________   | 
+   |    ____/ |______ |_____/ |     | |______  \_/   | \  | |           | 
+   |   /______|______ |    \_ |_____|_______|   |    |  \_| |_____      | 
+   |                                                                    | 
+   ======================================================================
+    _     _ _______  _____   _____  __   __      __   _ _______ _  _  _  
+    |_____| |_____| |_____] |_____]   \_/        | \  | |______ |  |  |  
+    |     | |     | |       |          |         |  \_| |______ |__|__|  
+ 
+                    __   __ _______ _______  ______
+                      \_/   |______ |_____| |_____/
+                       |    |______ |     | |    \_
+                
+    "#;
+   
+   println!("{}", new_years_message);
+   println!("CHAINSTATE DELIVERY FROM SPACE\n {}", serde_json::to_string_pretty(&chain_state.unwrap()).unwrap());
 
    Ok(())
 }
